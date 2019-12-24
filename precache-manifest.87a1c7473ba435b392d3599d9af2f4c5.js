ebd544a7af9c0410f80a432cae41f935e2956ee5
MasindeOdera <MasindeOdera@gmail.com>
2019-12-24 13:06:21 +0700

    Updates

---
+++ precache-manifest.87a1c7473ba435b392d3599d9af2f4c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2d5f573f852d97cb220feb8915d72ce",
+    "url": "/list/index.html"
+  },
+  {
+    "revision": "768dfa881fc1a7698f34",
+    "url": "/list/static/css/main.01721e5b.chunk.css"
+  },
+  {
+    "revision": "16f45490362f69ff03bb",
+    "url": "/list/static/js/2.1e724b3c.chunk.js"
+  },
+  {
+    "revision": "768dfa881fc1a7698f34",
+    "url": "/list/static/js/main.6bebaeb9.chunk.js"
+  },
+  {
+    "revision": "1e3d4d1129e59859c8a5",
+    "url": "/list/static/js/runtime-main.d5a776a7.js"
+  }
+]);
